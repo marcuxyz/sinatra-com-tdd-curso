96072611ed09906d9690de0c0deaaeafc4a16377
Marcus Pereira <mpereirassa@gmail.com>
2021-02-06 12:58:26 -0300

    delete post

--- spec/features/admin_delete_post_spec.rb
+++ spec/features/admin_delete_post_spec.rb
@@ -1,15 +1,14 @@
 feature 'Admin delete post' do
-  scenario 'with successfully' do
+  scenario 'with successfully', js: true do
     category = Category.create(name: 'Ação')
     user = User.create(name: 'Marcus Pereira',
                        email: 'preto@marcuspereira.xyz', password: '123456')
     post = Post.create(title: 'Playstation 5 custa 5k', content: 'lorem example im',
                        category: category, status: 'publicado', user: user)
 
-    visit "/post/#{post.id}"
-    click_on 'Deletar'
+    visit "/"
+    click_button(id: "delete-#{post._id}")
 
-    expect(current_path).to eq('/')
     expect(page).not_to have_content('Playstation 5 custa 5k')
   end
 end
