4acd69188c152397938221465832e98c9e32dd64
Marcus Pereira <mpereirassa@gmail.com>
2021-02-05 23:04:35 -0300

    spec_rails to spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,20 +1,23 @@
-require 'rack/test'
+require 'capybara'
 require 'capybara/rspec'
+require 'capybara/dsl'
 require 'rspec'
+require 'rack/test'
 require 'require_all'
 require 'mongoid'
 
-require_all('app/controllers')
 require_all('app/models')
-
 Mongoid.load!('config/mongoid.yaml', :testing)
 
 RSpec.configure do |config| 
+  config.include Capybara::DSL
   config.before(:each) do
     Mongoid.purge!
   end
 end
 
-module RSpecMixin
-  include Rack::Test::Methods
+def app
+  Rack::Builder.parse_file('config.ru').first
 end
+
+Capybara.app = app
