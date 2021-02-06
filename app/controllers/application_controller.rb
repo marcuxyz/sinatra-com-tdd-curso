d4de2ac25c2c441b3a0963d636f422b1627b5b3f
Marcus Pereira <mpereirassa@gmail.com>
2021-02-06 12:59:54 -0300

    register sinatra reloader

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 require 'sinatra'
+require 'sinatra/contrib'
 
 class ApplicationController  < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+
   configure do
     set :views, 'app/views'
   end
