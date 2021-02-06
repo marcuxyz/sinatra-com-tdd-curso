7d6163a73adfe90994096caa788f63779f38fa2e
Marcus Pereira <mpereirassa@gmail.com>
2021-02-06 20:59:34 -0300

    change is_authenticated? to authenticated?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,15 +16,15 @@ class ApplicationController < Sinatra::Base
 
   helpers do
     def current_user
-      return User.find(session[:user_id]) if is_authenticated?
+      return User.find(session[:user_id]) if authenticated?
     end
 
-    def is_authenticated?
+    def authenticated?
       session[:user_id] ? true : false
     end
 
     def protected!
-      halt 401, "Sem autorização\n" unless is_authenticated?
+      halt 401, "Sem autorização\n" unless authenticated?
     end
   end
 end
