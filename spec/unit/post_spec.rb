d9d7001a98cc2ac8c2784bd0d20709b6aa52f7a1
Marcus Pereira <mpereirassa@gmail.com>
2021-02-06 11:18:00 -0300

    create unit spec for post

---
+++ spec/unit/post_spec.rb
@@ -0,0 +1,26 @@
+describe 'Post Model'  do
+  it 'create new post' do
+    category = Category.create(
+      name: 'FPS'
+    )
+    user = User.create(
+      name: 'Marcus Pereira',
+      email: 'preto@marcuspereira.xyz',
+      password: '123456'
+    )
+    post = Post.new(
+      title: 'novo mapa de call of duty é lançado',
+      content: 'o map caspin berry foi adaptado para o novo cold war',
+      status: 'publicado',
+      category: category,
+      user: user
+    )
+
+    post.save!
+
+    expect(post.title).to eq('novo mapa de call of duty é lançado')
+    expect(post.content).to eq('o map caspin berry foi adaptado para o novo cold war')
+    expect(post.status).to eq('publicado')
+    expect(post.category.name).to eq('FPS')
+  end
+end
