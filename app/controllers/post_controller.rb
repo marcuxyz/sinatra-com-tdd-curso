50d85874514a9f8a214c9413b488775b52b02b28
Marcus Pereira <mpereirassa@gmail.com>
2021-02-05 23:05:19 -0300

    admin views post spec

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -12,4 +12,19 @@ class PostController < ApplicationController
     end
     erb :post
   end
+
+  get '/postagem/nova' do
+    @user = User.first
+    erb :new_post
+  end
+
+  post '/post/enviar' do
+    @post = Post.new(params)
+    byebug
+    if @post.save
+      return redirect to '/'
+    end
+
+    erb :new_post
+  end
 end
--- spec/features/admin_views_post_spec.rb
+++ spec/features/admin_views_post_spec.rb
@@ -1,10 +1,7 @@
-feature 'Admin' do
-  background do
-    Capybara.app = PostController
-  end
+feature 'Admin' do 
   scenario 'delete post' do
     user = User.create(name: 'Marcus Pereira',
-                      email: 'preto@marcuspereira.xyz', password: '123456')
+                       email: 'preto@marcuspereira.xyz', password: '123456')
     post = Post.create(title: 'Playstation 5 custa 5k', content: 'lorem example im', 
                        category: 'Games', status: 'publicado', user_id: user.id)
 
@@ -14,4 +11,24 @@ feature 'Admin' do
     expect(current_path).to eq('/')
     expect(page).not_to have_content('Playstation 5 custa 5k')
   end
+  
+  scenario 'register new post' do
+    User.create(name: 'Marcus Pereira',
+                email: 'preto@marcuspereira.xyz', password: '123456')
+    visit "/"
+    click_on 'Nova Postagem'
+
+    fill_in 'Título', with: 'Ações da Nintendo Aumenta 30% após Super Metroid'
+    fill_in 'Conteúdo', with: 'Nintedo dobra o valor das ações'
+    select 'Ação', from: 'Categorias'
+    select 'Publicado', from: 'Status'
+    select 'Marcus Pereira', from: 'Autor'
+    click_on 'Cadastrar'
+
+    expect(current_path).to eq('/')
+    expect(page).to have_link('Ações da Nintendo Aumenta 30% após Super Metroid')
+    expect(page).to have_content('1')
+    expect(page).to have_content('1')
+  end
+
 end
